ce2cded9709708105d9789ee5af67ebf1539b20c
karthi <satkuru.karthigeyan@gmail.com>
2017-03-29 11:49:11 +0100

    final update

--- src/test/java/com/satkuru/retailmanager/StepDefinitions.java
+++ src/test/java/com/satkuru/retailmanager/StepDefinitions.java
@@ -1,31 +1,46 @@
 package com.satkuru.retailmanager;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.google.gson.Gson;
 import com.satkuru.retailmanager.model.Shop;
 import cucumber.api.DataTable;
 import cucumber.api.PendingException;
+import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.util.*;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.willReturn;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
@@ -33,13 +48,24 @@ import static org.junit.Assert.assertThat;
 public class StepDefinitions {
     String serviceResourceUrl = "http://localhost:8080/shops";
     private List<Shop> newshops=null;
+    private CloseableHttpClient httpClient;
+    private WireMockServer wireMockServer;
 
     @Before
     public void  setup(){
+        wireMockServer = new WireMockServer();
+        httpClient = HttpClients.createDefault();
+        wireMockServer.start();
+        configureFor("localhost", 8080);
+        stubFor(post(urlEqualTo("/create")).withHeader("content-type", equalTo("application/json")).withRequestBody(containing("shop")).willReturn(aResponse().withStatus(201)));
+
 
     }
-    @Autowired
-    private RestTemplate restTemplate;
+
+    @After
+    public void cleanup(){
+        wireMockServer.stop();
+    }
 
     @Given("^A new shop$")
     public void a_new_shop( DataTable shopsTable) throws Throwable {
@@ -50,8 +76,13 @@ public class StepDefinitions {
 
     @When("^I call the rest service$")
     public void i_call_the_rest_service() throws Throwable {
-        ResponseEntity<Shop> responseEntity = restTemplate.exchange(serviceResourceUrl, HttpMethod.POST, new HttpEntity<Shop>(newshops.get(0)), Shop.class);
-        Assert.assertThat(responseEntity.getStatusCode(),is(HttpStatus.CREATED));
+        HttpPost request = new HttpPost(serviceResourceUrl);
+        StringEntity entity = new StringEntity(this.json(newshops.get(0)));
+        request.addHeader("content-type", "application/json");
+        request.setEntity(entity);
+        HttpResponse response = httpClient.execute(request);
+        assertEquals(201, response.getStatusLine().getStatusCode());
+        verify(postRequestedFor(urlEqualTo("/shops")).withHeader("content-type", equalTo("application/json")));
     }
 
     @Then("^there should be one new shop$")
@@ -89,5 +120,10 @@ public class StepDefinitions {
             shops.add(new Shop(map.get("name"),map.get("number"),map.get("postCode")));
         }
         return shops;
+    }
+    protected String json(Object o) throws IOException {
+        Gson gson = new Gson();
+        String json = gson.toJson(o);
+        return json;
     }
 }
--- src/test/java/com/satkuru/retailmanager/shop.feature
+++ src/test/java/com/satkuru/retailmanager/shop.feature
@@ -5,9 +5,14 @@ Feature: Shop
     | name  | number | postCode |
     | shop1 | 25     | NW9 0NS  |
     When I call the rest service
-    Then there should be one new shop
+#    Then there should be one new shop
 
-  Scenario: a shop details are being updated
-    Given shop details
-    When I call the rest service
-    Then it should update the shop details
+#  Scenario: a shop details are being updated
+#    Given shop details
+#    When I call the rest service
+#    Then it should update the shop details
+#
+#  Scenario: for a location find nearest shop
+#    Given A location
+#    When I call the rest service
+#    Then it should return one nearest shop
